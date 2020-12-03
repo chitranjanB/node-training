ea0a7eb09bb1e866a64d938cd4bb7af866165d76
Chitranjan Babu <chitranjan.pc8@gmail.com>
2020-12-03 15:01:29 +0530

    learned os and emmitter module

--- app.js
+++ app.js
@@ -16,3 +16,18 @@ fs.readdir("./", function (err, files) {
     // console.log("result is ", files);
   }
 });
+
+const EventEmitter = require("events");
+const emitter = new EventEmitter();
+
+//event declaration should be first in the order
+emitter.on("log", function (arg) {
+  console.log("event has raised", arg);
+});
+
+// event raised
+emitter.emit("log", {
+  id: 1,
+  url: "http://abc.com/log",
+  body: { message: "success", timestamp: "12/12/20" },
+});
