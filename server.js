d09822da49f9649168cba062e5916011f8becd66
Chitranjan Babu <chitranjan.pc8@gmail.com>
2020-12-03 20:02:33 +0530

    http module - handle request and response

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const http = require("http");
 
-const server = http.createServer();
-
-//socket based handling
-server.on("connection", function (socket) {
-  console.log("new connection");
+const server = http.createServer(function (req, res) {
+  if (req.url === "/") {
+    res.write(
+      "<html><title>Awesome</title><body><h1>Hello World</h1></body></html>"
+    );
+    res.end();
+  } else if (req.url === "/v1/api/courses") {
+    res.write(
+      JSON.stringify([
+        { courseName: "Node", courseTitle: "Node for beginners" },
+        { courseName: "React", courseTitle: "React design patterns" },
+      ])
+    );
+    res.end();
+  }
 });
 
 server.listen(3000);
