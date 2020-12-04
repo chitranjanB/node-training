7aa46550a9b234dcfac507f4cb86783c6bc91112
Chitranjan Babu <chitranjan.pc8@gmail.com>
2020-12-04 20:58:44 +0530

    express - put operation

--- index.js
+++ index.js
@@ -42,6 +42,28 @@ app.post("/api/courses", function (req, res) {
   res.status(201).send(newCourse);
 });
 
+app.put("/api/courses/:id", function (req, res) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+  const result = schema.validate(req.body);
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+  }
+
+  const courseId = req.params.id;
+  let updatedCourse = { id: parseInt(courseId), name: req.body.name };
+  const course = courses.find((c) => c.id === parseInt(updatedCourse.id));
+  if (!course) {
+    res.status(404).send(`No Course exists by id ${courseId}`);
+    return;
+  }
+
+  courses = courses.map((c) => (c.id === updatedCourse.id ? updatedCourse : c));
+  console.log("courses after update ", courses);
+  res.status(200).send(updatedCourse);
+});
+
 app.get("/api/posts/:year/:month", function (req, res) {
   res.send(req.params);
 });
