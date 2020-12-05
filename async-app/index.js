8200506d4fdbe461c7bc91abc3cb9974cff13e09
Chitranjan Babu <chitranjan.pc8@gmail.com>
2020-12-05 19:51:50 +0530

    async - promise chaining

--- async-app/index.js
+++ async-app/index.js
@@ -1,34 +1,40 @@
 console.log("before");
 
-getUser(1, function (user) {
-  getRepos(user.githubUser, function (repos) {
-    getCommits(repos[0], function (commits) {});
-  });
-});
+getUser(1)
+  .then((user) => getRepos(user.githubUser))
+  .then((repos) => getCommits(repos[0]))
+  .then((commits) => console.log("commits", commits));
+
 console.log("after");
 
-function getUser(id, callback) {
-  setTimeout(function () {
-    console.log("reading user from database");
-    callback({
-      id,
-      githubUser: "chitranjanB",
-    });
-  }, 2000);
+function getUser(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("reading user from database");
+      resolve({
+        id,
+        githubUser: "chitranjanB",
+      });
+    }, 2000);
+  });
 }
 
-function getRepos(username, callback) {
-  setTimeout(function () {
-    console.log("Fetching repos for user", username);
-    const repos = ["repo1", "repo2"];
-    callback(repos);
-  }, 2000);
+function getRepos(username) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Fetching repos for user", username);
+      const repos = ["repo1", "repo2"];
+      resolve(repos);
+    }, 2000);
+  });
 }
 
-function getCommits(repo, callback) {
-  setTimeout(function () {
-    console.log("Fetching commits for repo", repo);
-    const commits = ["commit1", "commit2"];
-    callback(commits);
-  }, 2000);
+function getCommits(repo) {
+  return new Promise((resolve, reject) => {
+    setTimeout(function () {
+      console.log("Fetching commits for repo", repo);
+      const commits = ["commit1", "commit2"];
+      resolve(commits);
+    }, 2000);
+  });
 }
