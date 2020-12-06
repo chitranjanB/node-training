e33cc08cc2aa757d740e9a856b250a37ab0e1f52
Chitranjan Babu <chitranjan.pc8@gmail.com>
2020-12-06 20:50:18 +0530

    mongo - customer api updated mongoose

---
+++ vidly-customer/routes/customers.js
@@ -0,0 +1,86 @@
+const Joi = require("joi");
+const mongoose = require("mongoose");
+const express = require("express");
+const router = express.Router();
+
+const Customer = mongoose.model(
+  "Customer",
+  mongoose.Schema({
+    isGold: Boolean,
+    name: { type: String, required: true, minlength: 4, maxlength: 20 },
+    phone: { type: String, required: true, minlength: 5, maxlength: 10 },
+  })
+);
+
+router.get("/", async (req, res) => {
+  const customers = await Customer.find().sort("name");
+  res.send(customers);
+});
+
+router.post("/", async (req, res) => {
+  const { error } = validateCustomer(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const customer = new Customer({
+    isGold: req.body.isGold,
+    name: req.body.name,
+    phone: req.body.phone,
+  });
+  await customer.save();
+  res.send(customer);
+});
+
+router.put("/:id", async (req, res) => {
+  const { error } = validateCustomer(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const customer = await Customer.findByIdAndUpdate(
+    req.params.id,
+    {
+      isGold: req.body.isGold,
+      name: req.body.name,
+      phone: req.body.phone,
+    },
+    { new: true }
+  );
+
+  if (!customer)
+    return res
+      .status(404)
+      .send("The customer with the given ID was not found.");
+
+  res.send(customer);
+});
+
+router.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+  const customer = await Customer.findByIdAndRemove(id);
+  if (!customer)
+    return res
+      .status(404)
+      .send("The customer with the given ID was not found.");
+
+  res.send(customer);
+});
+
+router.get("/:id", async (req, res) => {
+  const customer = await Customer.findById(req.params.id);
+  if (!customer)
+    return res
+      .status(404)
+      .send("The customer with the given ID was not found.");
+
+  res.send(customer);
+});
+
+function validateCustomer(customer) {
+  const schema = {
+    name: Joi.string().min(5).max(50).required(),
+    phone: Joi.string().min(5).max(50).required(),
+    isGold: Joi.boolean(),
+  };
+
+  return Joi.validate(customer, schema);
+}
+
+module.exports = router;
