49e5545f120deedec8a83178383b5d858dbf3f31
Chitranjan Babu <chitranjan.pc8@gmail.com>
2020-12-27 20:16:36 +0530

    Created a middleware function to reduce error logging boiler plate

--- vidly-authenticate/routes/register.js
+++ vidly-authenticate/routes/register.js
@@ -3,17 +3,29 @@ const _ = require("lodash");
 const router = express.Router();
 const { User, validateUser } = require("../models/user");
 
-router.post("/", async function (req, res) {
-  const { error } = validateUser(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+function asyncMiddleware(handler) {
+  return async function (req, res, next) {
+    try {
+      await handler(req, res);
+    } catch (err) {
+      next(err);
+    }
+  };
+}
+router.post(
+  "/",
+  asyncMiddleware(async function (req, res) {
+    const { error } = validateUser(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
 
-  try {
-    let user = new User(_.pick(req.body, ["name", "email", "password"]));
-    user = await user.save();
-    res.send(user);
-  } catch (error) {
-    next(error);
-  }
-});
+    try {
+      let user = new User(_.pick(req.body, ["name", "email", "password"]));
+      user = await user.save();
+      res.send(user);
+    } catch (error) {
+      next(error);
+    }
+  })
+);
 
 module.exports = router;
